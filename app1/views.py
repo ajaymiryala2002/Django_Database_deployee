8c0f9cc4a464bc6e6dba85f3701c84f4051f5776
ajaymiryala2002 <ajaymiryala259@gmail.com>
2025-11-18 21:59:52 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
+from .forms import StudentForm
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, "student_list.html", {"students": students})
+
+def student_create(request):
+    form = StudentForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect("student_list")
+    return render(request, "student_form.html", {"form": form})
+
+# def student_update(request, id):
+#     student = get_object_or_404(Student, id=id)
+#     form = StudentForm(request.POST or None, instance=student)
+#     if form.is_valid():
+#         form.save()
+#         return redirect("student_list")
+#     return render(request, "students/student_form.html", {"form": form})
+
+# def student_delete(request, id):
+#     student = get_object_or_404(Student, id=id)
+#     if request.method == "POST":
+#         student.delete()
+#         return redirect("student_list")
+#     return render(request, "students/student_delete.html", {"student": student})
