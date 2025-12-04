e18aadfba7966cc86f56f17440240b77e40e1b11
ajaymiryala2002 <ajaymiryala259@gmail.com>
2025-12-04 22:12:42 +0530

    Test commit

--- django_db_curd/settings.py
+++ django_db_curd/settings.py
@@ -96,28 +96,23 @@ WSGI_APPLICATION = 'django_db_curd.wsgi.application'
 #         }
 #     }
     
-# }
-
-" "
-
 import os
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST','127.0.0.1'),
-        'PORT': os.getenv('DB_PORT','3306'),
+        'NAME': os.getenv('DB_NAME', 'my_default_db'),
+        'USER': os.getenv('DB_USER', 'root'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
             'ssl': {
-                'ca': os.getenv('DATABASE_SSL_CERT')
+                'ca': os.getenv('DATABASE_SSL_CERT', None)  # optional
             }
         }
     }
 }
-
 
 
 
